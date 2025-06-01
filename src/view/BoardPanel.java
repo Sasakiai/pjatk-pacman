284f023cbb6ed9d0562e1f69c6a4a90b676067ce
Sasakiai <szpakowiczk@gmail.com>
2025-06-02 00:18:13 +0200

    Board coloring

--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -1,9 +1,11 @@
 package view;
 
+import enums.TileType;
 import model.BoardModel;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 
 public class BoardPanel extends JPanel {
@@ -11,20 +13,53 @@ public class BoardPanel extends JPanel {
 
     public BoardPanel(BoardModel model) {
         board = new JTable(model);
-        board.setRowHeight(32);
+
+        // disable any interaction
         board.setFocusable(false);
         board.setCellSelectionEnabled(false);
         board.setColumnSelectionAllowed(false);
         board.setDragEnabled(false);
 
+        // display settings
+        board.setRowHeight(32);
+        board.setIntercellSpacing(new Dimension(0, 0));
+        board.setTableHeader(null);
+        board.setShowGrid(false);
+
+        for (int i = 0; i < board.getColumnCount(); i++) {
+            TableColumn col = board.getColumnModel().getColumn(i);
+
+            col.setMinWidth(32);
+            col.setMaxWidth(32);
+            col.setPreferredWidth(32);
+        }
+
+        board.setDefaultRenderer(Object.class, new CellRenderer());
 
         add(board);
     }
 
-//    private class CellRenderer extends DefaultTableCellRenderer {
-//        @Override
-//        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-//
-//        }
-//    }
+    private class CellRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            JPanel tile = new JPanel();
+            tile.setOpaque(true);
+
+            if (value instanceof TileType) {
+                System.out.println(value);
+                switch ((TileType) value) {
+                    case EMPTY:
+                        tile.setBackground(Color.BLACK);
+                        break;
+                    case WALL:
+                        tile.setBackground(Color.BLUE);
+                        break;
+                    default:
+                        tile.setBackground(Color.BLACK);
+                }
+            }
+
+            return tile;
+        }
+    }
 }
